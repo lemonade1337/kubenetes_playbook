b43983177e725cc1aefabf3f9165d042b96dc9cd
Alex <alexroesler@gmx.de>
2026-09-25 09:25:29 +0200

    llm pod deployment improvement

--- roles/api_gateway_portal/files/app/database.py
+++ roles/api_gateway_portal/files/app/database.py
@@ -13,7 +13,7 @@ POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres-service")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "token_portal")
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(
     DATABASE_URL,
